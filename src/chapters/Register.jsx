b310770404d3f6c1092f1b4144fa7cb41a92252c
jainanmol13433 <52275941+jainanmol13433@users.noreply.github.com>
2022-12-03 22:50:33 +0530

    Update Register.jsx

--- src/chapters/Register.jsx
+++ src/chapters/Register.jsx
@@ -16,7 +16,7 @@ const Register = () => {
   e.preventDefault()
     if(validation()){
       const {Username,Email,Password}=keys;
-      const {data}=await axios.post(`/api/register`,{
+      const {data}=await axios.post(`https://chatset.onrender.com/register`,{
         Username,
         Email,
         Password
